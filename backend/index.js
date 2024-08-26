3021cdf340b0bf97275b56f9b148160b043026c2
salman_siddeeque <salmanres@gmail.com>
2024-08-27 02:05:15 +0530

    salman

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://app2-kqzp.onrender.com",
     credentials: true
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
